b240b7d865a9722e841aaab72007a900cb546139
Aditya Fajri <aditya.fajri@binus.edu>
2025-11-10 05:38:07 +0700

    fix: update MQTT configuration to use correct environment variable names

--- src/core/config/mqtt.config.ts
+++ src/core/config/mqtt.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('mqtt', () => ({
   url: process.env.MQTT_URL || 'mqtt://localhost:1883',
-  username: process.env.MQTT_USERNAME || '',
-  password: process.env.MQTT_PASSWORD || '',
+  username: process.env.MQTT_USER || '',
+  password: process.env.MQTT_PASS || '',
   clientId: `smartcanopy-api-client-${Math.random().toString(16).slice(3)}`,
 }));
